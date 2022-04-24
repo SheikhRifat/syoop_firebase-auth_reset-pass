ebcd7417fafbd518fd56f30851a741d9c102bee8
Tawhidul Arefin <tawhidularefin530@gmail.com>
2022-04-25 01:44:57 +0600

    Added: Singlechildscroolview

--- lib/src/auth/entrepreneur/signup.dart
+++ lib/src/auth/entrepreneur/signup.dart
@@ -11,122 +11,126 @@ class Signup extends StatelessWidget {
     return Scaffold(
       resizeToAvoidBottomInset: false,
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 30),
-        child: Column(
-          children: [
-            SizedBox(
-              height: 30,
-            ),
-            Center(
-              child: Container(
-                height: 150,
-                width: 150,
-                decoration: BoxDecoration(
+      body: Container(
+        height: Get.height,
+        width: Get.width,
+        padding: EdgeInsets.symmetric(horizontal: 15),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 30,
+              ),
+              Center(
+                child: Container(
+                  height: 150,
+                  width: 150,
+                  decoration: BoxDecoration(
+                    color: HexColor('#0025A8'),
+                    shape: BoxShape.circle,
+                  ),
+                  child: Center(
+                    child: KText(
+                      text: 'SYOOP',
+                      color: Colors.white,
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              KText(
+                text: 'Syoop',
+                fontSize: 30,
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
+              TextField(
+                decoration: kInputDecoration(hintText: 'First name'),
+              ),
+              TextField(
+                decoration: kInputDecoration(hintText: 'Last name'),
+              ),
+              TextField(
+                decoration: kInputDecoration(hintText: 'Email'),
+              ),
+              TextField(
+                decoration: kInputDecoration(hintText: 'Password'),
+              ),
+              TextField(
+                decoration: kInputDecoration(hintText: 'Website[optiona]'),
+              ),
+              TextField(
+                decoration:
+                    kInputDecoration(hintText: 'Topic that interest you?'),
+              ),
+              TextField(
+                decoration: kInputDecoration(hintText: 'User name'),
+              ),
+              TextField(
+                decoration: kInputDecoration(hintText: 'App name'),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              SizedBox(
+                height: 50,
+                width: size.width * 0.50,
+                child: ElevatedButton(
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(
+                          HexColor('#0025A8'),
+                        ),
+                        shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(40)))),
+                    onPressed: () {},
+                    child: KText(
+                      text: 'Submit',
+                      color: Colors.white,
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                    )),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+                Icon(
+                  Icons.verified_user_outlined,
+                  size: 15,
                   color: HexColor('#0025A8'),
-                  shape: BoxShape.circle,
                 ),
-                child: Center(
-                  child: KText(
-                    text: 'SYOOP',
-                    color: Colors.white,
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
+                Container(
+                  child: RichText(
+                    text: TextSpan(
+                      text: "Agree to our ",
+                      style: TextStyle(color: Colors.black),
+                      children: <TextSpan>[
+                        TextSpan(
+                          text: "Terms of service ",
+                          style: TextStyle(
+                            color: HexColor('#0025A8'),
+                          ),
+                        ),
+                        TextSpan(
+                          text: "and ",
+                          style: TextStyle(
+                            color: Colors.black,
+                          ),
+                        ),
+                        TextSpan(
+                          text: "privacy policy",
+                          style: TextStyle(
+                            color: HexColor('#0025A8'),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-              ),
-            ),
-            KText(
-              text: 'Syoop',
-              fontSize: 30,
-              color: Colors.black,
-              fontWeight: FontWeight.bold,
-            ),
-            TextField(
-              decoration: kInputDecoration(hintText: 'First name'),
-            ),
-            TextField(
-              decoration: kInputDecoration(hintText: 'Last name'),
-            ),
-            TextField(
-              decoration: kInputDecoration(hintText: 'Email'),
-            ),
-            TextField(
-              decoration: kInputDecoration(hintText: 'Password'),
-            ),
-            TextField(
-              decoration: kInputDecoration(hintText: 'Website[optiona]'),
-            ),
-            TextField(
-              decoration:
-                  kInputDecoration(hintText: 'Topic that interest you?'),
-            ),
-            TextField(
-              decoration: kInputDecoration(hintText: 'User name'),
-            ),
-            TextField(
-              decoration: kInputDecoration(hintText: 'App name'),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            SizedBox(
-              height: 50,
-              width: size.width * 0.50,
-              child: ElevatedButton(
-                  style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(
-                        HexColor('#0025A8'),
-                      ),
-                      shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(40)))),
-                  onPressed: () {},
-                  child: KText(
-                    text: 'Submit',
-                    color: Colors.white,
-                    fontSize: 22,
-                    fontWeight: FontWeight.bold,
-                  )),
-            ),
-            SizedBox(
-              height: 5,
-            ),
-            Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
-              Icon(
-                Icons.verified_user_outlined,
-                size: 15,
-                color: HexColor('#0025A8'),
-              ),
-              Container(
-                child: RichText(
-                  text: TextSpan(
-                    text: "Agree to our ",
-                    style: TextStyle(color: Colors.black),
-                    children: <TextSpan>[
-                      TextSpan(
-                        text: "Terms of service ",
-                        style: TextStyle(
-                          color: HexColor('#0025A8'),
-                        ),
-                      ),
-                      TextSpan(
-                        text: "and ",
-                        style: TextStyle(
-                          color: Colors.black,
-                        ),
-                      ),
-                      TextSpan(
-                        text: "privacy policy",
-                        style: TextStyle(
-                          color: HexColor('#0025A8'),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              )
-            ]),
-          ],
+                )
+              ]),
+            ],
+          ),
         ),
       ),
     );
